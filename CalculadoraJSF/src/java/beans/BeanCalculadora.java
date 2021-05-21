871bfac93c245fa9e0d6fc93d94d86f0d7f297dc
vilsonmoro <vilsonmoro@gmail.com>
2021-05-21 20:13:45 -0300

    Calculadora JSF

---
+++ CalculadoraJSF/src/java/beans/BeanCalculadora.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author vilso
+ */
+@ManagedBean
+public class BeanCalculadora {
+    private float n1;
+    private float n2;
+    private float resultado;
+   
+    public void somar(){
+        resultado = n1 + n2;
+    }
+    
+    public float getN1() {
+        return n1;
+    }
+
+    public void setN1(float n1) {
+        this.n1 = n1;
+    }
+
+    public float getN2() {
+        return n2;
+    }
+
+    public void setN2(float n2) {
+        this.n2 = n2;
+    }
+
+    public float getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(float resultado) {
+        this.resultado = resultado;
+    }
+    
+    
+}
