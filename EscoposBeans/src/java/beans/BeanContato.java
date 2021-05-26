64d3d6d7dca4287b891b07d789705bbd7cf4afcf
vilsonmoro <vilsonmoro@gmail.com>
2021-05-26 19:39:00 -0300

    Escopos de beans

---
+++ EscoposBeans/src/java/beans/BeanContato.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@SessionScoped
+public class BeanContato {
+    private String nome;
+    private String sobrenome;
+    private String nomeCompleto;
+
+    public void gerarNomeCompleto(){
+        nomeCompleto = nome + " " + sobrenome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }    
+}
