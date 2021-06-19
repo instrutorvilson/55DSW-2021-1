f976c6352b8c2893db86661b52361e16217a081b
vilsonmoro <vilsonmoro@gmail.com>
2021-06-18 21:07:24 -0300

    Criar metodo get para retornar um unico registro

--- RestAPI/src/java/services/Aula.java
+++ RestAPI/src/java/services/Aula.java
@@ -14,6 +14,8 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -25,17 +27,34 @@ public class Aula {
 
     @Context
     private UriInfo context;
- 
+
     public Aula() {
     }
 
-    
     @GET
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-    public String getJson() {
-       Gson gson = new Gson(); 
-       return gson.toJson(dao.DaoGeneric.getAll());
-      //return new Contato("José", "123", "jose");
-    }  
-    
+    public Response getAll() {
+        Gson gson = new Gson();
+        return Response.status(Response.Status.OK)
+                .entity(gson.toJson(dao.DaoGeneric.getAll()))
+                .build();
+        //return new Contato("José", "123", "jose");
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public Response getOne(@PathParam("id") long id) {
+        Contato ct = dao.DaoGeneric.getById(id);
+        if (ct == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                           .build();
+        } else {
+            return Response.status(Response.Status.FOUND)
+                    .entity(ct)
+                    .build();
+        }
+
+    }
+
 }
